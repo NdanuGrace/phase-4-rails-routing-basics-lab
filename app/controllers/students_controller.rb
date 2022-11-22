af5bc772dbb3b42da6de8a6b1d767b24949d5219
gracekyalo <gracendanu27@gmail.com>
2022-11-22 07:54:16 +0300

    Completed assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index
+        students=Student.all 
+        render json:students
+    end
+    def grades
+        students =Student.all.order(grade: :DESC)
+        render json:students
+    end
+    def highest_grade
+        students =Student.all.order(grade: :DESC).limit(1)
+        render json:students
+    end
+
+end
